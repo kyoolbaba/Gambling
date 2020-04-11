c90a6dfe52203d8c6c0d8f553435bebaec0b8d13
kyoolbaba <kyoolbaba@gmail.com>
2020-04-11 15:37:20 +0530

    added feature to find out whether gamblers got 50% lose or 50% profit

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,9 +1,11 @@
 #!binbash
 STAKE=100
 BET=1
+while [[ $STAKE -lt '150' &&  $STAKE -gt '50' ]]; do
 if [[ $((RANDOM%2)) -eq '0' ]]
 then
 	STAKE=$((STAKE-BET))
 else
 	STAKE=$((STAKE+BET))
 fi
+done
